53fff1e946dcb4d713356127242788cb6ed2c40f
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2023-11-06 09:23:41 -0500

    some stuff

---
+++ DCG/Scripts/Game Classes/Card.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+using System;
+
+public partial class Card : Node3D { 
+    private void getCard(string[] cards,int req)
+    {
+        var card = new Card();
+        Label3D cardName = (Label3D)NodePath("FrontFace/Name/Name");
+        cardName.Text = cards[req];
+    }
+}
+
