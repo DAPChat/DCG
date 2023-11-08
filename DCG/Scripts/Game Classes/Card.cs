378f6a776838fc25fd64069af593d914ad260586
Anthony <antsal07@gmail.com>
2023-11-08 07:22:03 -0500

    Formatting for readability

--- DCG/Scripts/Game Classes/Card.cs
+++ DCG/Scripts/Game Classes/Card.cs
@@ -11,16 +11,22 @@ public partial class Card : Node3D
     {
         var cardName = GetNode<Label>("FrontFace/Name/Name");
         cardName.Text = cards[req]["Name"];
+
         var cardRank = GetNode<Label>("FrontFace/Name/Rank");
         cardRank.Text = cards[req]["Rank"];
+
         var cardDescription = GetNode<Label>("FrontFace/Description/Description");
         cardDescription.Text = cards[req]["Description"];
+
         var cardStats = GetNode<Label>("FrontFace/Description/Stats");
         cardStats.Text = cards[req]["ATK"] + " ATK / " + cards[req]["HP"] + " HP";
+
         var newImage = ImageTexture.CreateFromImage(cards[req]["Image"]);
         var mesh = GetNode<MeshInstance3D>("FrontFace/Picture");
+
         var material = mesh.GetActiveMaterial(0) as StandardMaterial3D;
         material!.AlbedoTexture = newImage;
+
         //add switch case for the type of card
         //in switch case change background of card
     }
