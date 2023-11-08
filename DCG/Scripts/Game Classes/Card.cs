e86000b7eded3e7ba0b0c2baf34776472b1f1124
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2023-11-08 11:58:30 -0500

    stuff

--- DCG/Scripts/Game Classes/Card.cs
+++ DCG/Scripts/Game Classes/Card.cs
@@ -10,16 +10,16 @@ public partial class Card : Node3D
 
     public Card(dynamic[] cards, int req)
     {
-        var cardName = GetNode<Label>("FrontFace/Name/Name");
+        var cardName = GetNode<Label>("/FrontFace/Name/Name");
         cardName.Text = cards[req]["Name"];
-        var cardRank = GetNode<Label>("FrontFace/Name/Rank");
+        var cardRank = GetNode<Label>("/FrontFace/Name/Rank");
         cardRank.Text = cards[req]["Rank"];
-        var cardDescription = GetNode<Label>("FrontFace/Description/Description");
+        var cardDescription = GetNode<Label>("/FrontFace/Description/Description");
         cardDescription.Text = cards[req]["Description"];
-        var cardStats = GetNode<Label>("FrontFace/Description/Stats");
+        var cardStats = GetNode<Label>("/FrontFace/Description/Stats");
         cardStats.Text = cards[req]["ATK"] + " ATK / " + cards[req]["HP"] + " HP";
         var newImage = ImageTexture.CreateFromImage(getImg(cards[req]["Image"]));
-        var mesh = GetNode<MeshInstance3D>("FrontFace/Picture");
+        var mesh = GetNode<MeshInstance3D>("/FrontFace/Picture");
         var material = mesh.GetActiveMaterial(0) as StandardMaterial3D;
         material!.AlbedoTexture = newImage;
         if (cards[req]["Type"])
@@ -38,7 +38,7 @@ public partial class Card : Node3D
                     break;
             }
             var newImageBg = ImageTexture.CreateFromImage(getImg(imageBg));
-            var meshBg = GetNode<MeshInstance3D>("FrontFace");
+            var meshBg = GetNode<MeshInstance3D>("/FrontFace");
             var materialBg = meshBg.GetActiveMaterial(0) as StandardMaterial3D;
             materialBg!.AlbedoTexture = newImageBg;
         }
--- DCG/Scripts/Game Classes/Main.cs
+++ DCG/Scripts/Game Classes/Main.cs
@@ -1,11 +1,13 @@
 ﻿using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
 using static System.Net.Mime.MediaTypeNames;
 
-class Program
+public partial class Main : Node3D 
 {
-    class CardObject
+    public class CardObject
     {
         public string? Name { get; set; }
         public string? Rank { get; set; }
@@ -15,9 +17,18 @@ class Program
         public int HP { get; set; }
         public string? Image { get; set; }
     }
-    static void Main(string[] args)
+    public void Run ()
     {
-        dynamic[] dict = [new CardObject { Name = "random", Rank = "S", Description = "akdsjfhlaksdfh", Type = "Spell", ATK = 10000, HP = 10000, Image = "https://publicfiles.dapchat.repl.co/dcgtest/reaper.jpg" }];
-        Card cardtest = new(dict, 0);
+        List<CardObject> list = new List<CardObject>();
+        list.Append(new CardObject { Name = "random", Rank = "S", Description = "akdsjfhlaksdfh", Type = "Spell", ATK = 10000, HP = 10000, Image = "https://publicfiles.dapchat.repl.co/dcgtest/reaper.jpg" });
+        Card cardtest = new(list.ToArray(), 0);
+
+        var c = GD.Load<Node3D>("res://Scenes/card.tscn");
+        AddChild(c);
+    }
+
+    public override void _Ready()
+    {
+        Run();
     }
 }
