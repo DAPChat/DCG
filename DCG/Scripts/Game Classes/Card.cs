0e00c20e67d5b4a3f3ec097fe12127fd5936bfa7
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2023-11-08 09:23:17 -0500

    card thing

--- DCG/Scripts/Game Classes/Card.cs
+++ DCG/Scripts/Game Classes/Card.cs
@@ -5,9 +5,10 @@ using static System.Net.Mime.MediaTypeNames;
 
 public partial class Card : Node3D
 {
+
     //when using this you will do var newcard = new Card(dictionary,cardnum)
 
-    public Card(Dictionary<dynamic, dynamic> cards, int req)
+    public Card(dynamic[] cards, int req)
     {
         var cardName = GetNode<Label>("FrontFace/Name/Name");
         cardName.Text = cards[req]["Name"];
@@ -17,11 +18,42 @@ public partial class Card : Node3D
         cardDescription.Text = cards[req]["Description"];
         var cardStats = GetNode<Label>("FrontFace/Description/Stats");
         cardStats.Text = cards[req]["ATK"] + " ATK / " + cards[req]["HP"] + " HP";
-        var newImage = ImageTexture.CreateFromImage(cards[req]["Image"]);
+        var newImage = ImageTexture.CreateFromImage(getImg(cards[req]["Image"]));
         var mesh = GetNode<MeshInstance3D>("FrontFace/Picture");
         var material = mesh.GetActiveMaterial(0) as StandardMaterial3D;
         material!.AlbedoTexture = newImage;
-        //add switch case for the type of card
-        //in switch case change background of card
+        if (cards[req]["Type"])
+        {
+            string imageBg;
+            switch (cards[req]["Type"])
+            {
+                case "Spell":
+                    imageBg = "";
+                    break;
+                case "Fighter":
+                    imageBg = "";
+                    break;
+                default:
+                    imageBg = "";
+                    break;
+            }
+            var newImageBg = ImageTexture.CreateFromImage(getImg(imageBg));
+            var meshBg = GetNode<MeshInstance3D>("FrontFace");
+            var materialBg = meshBg.GetActiveMaterial(0) as StandardMaterial3D;
+            materialBg!.AlbedoTexture = newImageBg;
+        }
+    }
+
+    private Godot.Image getImg(string url)
+    {
+        HttpRequest request = new HttpRequest();
+        AddChild(request);
+        Godot.Image img = new Godot.Image();
+        request.RequestCompleted += (_result, responsecode, header, body) =>
+        {
+            Error error = img.LoadJpgFromBuffer(body);
+        };
+        Error error = request.Request(url);
+        return img;
     }
 }
---
+++ DCG/Scripts/Game Classes/Main.cs
@@ -0,0 +1,23 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    class CardObject
+    {
+        public string? Name { get; set; }
+        public string? Rank { get; set; }
+        public string? Description { get; set; }
+        public string? Type { get; set; }
+        public int ATK { get; set; }
+        public int HP { get; set; }
+        public string? Image { get; set; }
+    }
+    static void Main(string[] args)
+    {
+        dynamic[] dict = [new CardObject { Name = "random", Rank = "S", Description = "akdsjfhlaksdfh", Type = "Spell", ATK = 10000, HP = 10000, Image = "https://publicfiles.dapchat.repl.co/dcgtest/reaper.jpg" }];
+        Card cardtest = new(dict, 0);
+    }
+}
