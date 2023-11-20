280d515f1df02fe30c50bc88894a722f9c9481e2
Anthony <66685733+LEPA4@users.noreply.github.com>
2023-11-20 16:10:55 -0500

    Accounts

--- DCGServer/DCGServer/Game Classes/PlayerAccount.cs
+++ DCGServer/DCGServer/Game Classes/PlayerAccount.cs
@@ -7,15 +7,25 @@ public class PlayerAccount
 
 	public string username;
 	public string password;
+
 	public int rank;
 	public int streak;
+	public int totalWins;
+	public int totalLosses;
 
 	public string cardBack;
 	public string profilePicture;
 
-	public PlayerAccount(string _username, string _password)
+    public string[] cardBacks;
+
+	public int[] cards;
+	public int[][] decks;
+
+    public PlayerAccount(string _username, string _password)
 	{
 		username = _username;
 		password = _password;
+
+		decks = new int[3][];
 	}
 }
