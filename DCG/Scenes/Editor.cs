40740f0114d5a62b252803eaa3c11fd87cdb7cc0
Dominik Honzak <49328567+darkninja1@users.noreply.github.com>
2023-12-13 08:55:14 -0500

    really bad tab changing and some other stuff

---
+++ DCG/Scenes/Editor.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Editor : Control
+{
+	static Button AddCards;
+    static GridContainer TheArea;
+	public override void _Ready()
+	{
+        AddCards = (Button)GetNode("Home/UI/Editor/Add");
+        AddCards.Pressed += () => LoadCardsIn();
+    }
+    public void LoadCardsIn()
+    {
+    
+    }
+}
