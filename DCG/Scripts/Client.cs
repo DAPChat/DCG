94f7ff6d6a03918b3052f4bf64aba7e545e7e142
Anthony <66685733+LEPA4@users.noreply.github.com>
2024-01-09 16:47:12 -0500

    returned for testing

--- DCG/Scripts/Client.cs
+++ DCG/Scripts/Client.cs
@@ -15,7 +15,7 @@ public class Client
 	public TcpClient client = null;
 	public PlayerAccount account = null;
 
-	private IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("71.191.51.200"), 60606);
+	private IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.1.1.0"), 60606);
 
 	public NetworkStream stream = null;
 	private byte[] buffer = new byte[8196];
